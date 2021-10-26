dde1e2e661313d9d19c41e3f5d31089eba528357
radunsb <radunsb@gmail.com>
2021-10-26 12:33:44 -0400

    Revert "hi again"
    This reverts commit f74026f6d8e09daa44f8aa244312bec3145c00c0.

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@ public class Main {
     // Will probably handle running the title screen and the main game loop.
     // New stuff
     //i am adding a thing
+    //i am adding another thing, primarily to determine the difficulty of adding things wooo
 }

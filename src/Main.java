3c7a0caf51371a063108644bb4287d8b443da1ee
radunsb <radunsb@gmail.com>
2021-10-26 21:17:41 -0400

    I think I fixed some things. Made a sample class to make sure that some stuff works but should be deleted later.

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,13 @@ public class Main {
     // New stuff
     //i am adding a thing
     //i am adding another thing, primarily to determine the difficulty of adding things wooo
+    public static void main(String[] args) {
+        SampleClass meme = new SampleClass(55, "Jimmy");
+        System.out.println(meme.getNumber());
+        System.out.println(meme.getName());
+        meme.setName("Bob");
+        meme.setNumber(555);
+        System.out.println(meme.getNumber());
+        System.out.println(meme.getName());
+    }
 }
